b47daa3a38f5f07629680901f5afb1bd06dd694c
dircosine <zidhdy@gmail.com>
2020-04-27 03:03:52 +0900

    Set ACL of Output file to public-read

--- lambda_function.py
+++ lambda_function.py
@@ -10,12 +10,12 @@ import datetime
 AWS_ID = os.environ['AWS_ID']
 AWS_SECRET = os.environ['AWS_SECRET']
 
-CODES_FILE = 'codes-krx.csv'
-BUCKET = 'res-todaysstock'
-OUTPUT_FILE = datetime.datetime.now().strftime('%Y%m%d') + '_stock_infos.json'
-
 s3_client = boto3.client('s3', aws_access_key_id=AWS_ID,
                          aws_secret_access_key=AWS_SECRET)
+
+BUCKET = 'res-todaysstock'
+CODES_FILE = 'codes-krx.csv'
+OUTPUT_FILE = datetime.datetime.now().strftime('%Y%m%d') + '_stock_infos.json'
 
 N = 32
 
@@ -24,7 +24,7 @@ def lambda_handler(event, context):
     daily_info_array = []
     progress_index = 0
 
-    codes_obj = s3_client.get_object(Bucket=BUCKET, Key='codes-krx.csv')
+    codes_obj = s3_client.get_object(Bucket=BUCKET, Key=CODES_FILE)
     codes_body = codes_obj['Body']
     csv_string = codes_body.read().decode('utf-8')
 
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
     for row in csv_reader:
         codes.append(row)
 
-    daily_codes = random.sample(codes[1:], 50)
+    daily_codes = random.sample(codes[1:], 60)
 
     for code in daily_codes:
         progress_index += 1
@@ -52,6 +52,7 @@ def lambda_handler(event, context):
 
     s3_client.put_object(Body=json.dumps(
         daily_info_array, ensure_ascii=False), Bucket=BUCKET, Key=OUTPUT_FILE)
+    s3_client.put_object_acl(ACL='public-read', Bucket=BUCKET, Key=OUTPUT_FILE)
 
     print("Done!")
 
